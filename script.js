5147ba3fd88da5dfc2506e71d48afc70cb329359
DPO3 <98774344+DPO3@users.noreply.github.com>
2023-04-15 07:54:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let switchMode = document.getElementById("switchMode");
+
+switchMode.onclick = function() {
+    let theme = document.getElementById("theme");
+
+    if (theme.getAttribute("href") == "style.css")
+       { theme.href = "darkstyle.css"; 
+    } else {
+        theme.href = "style.css";
+    }
+} 
+
+
+
+
